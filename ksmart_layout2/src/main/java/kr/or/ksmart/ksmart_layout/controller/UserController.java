84db618d4bc83278a28bb7981ad554c24a7c5970
"PeaceNChicken" <kkjlevel33@gmail.com>
2019-08-09 00:37:58 +0900

    update 처리 완료 mybatis trim if 조건문 사용
    delete 화면 생성, url요청해서 삭제화면으로 이동까지 완료

--- ksmart_layout2/src/main/java/kr/or/ksmart/ksmart_layout/controller/UserController.java
+++ ksmart_layout2/src/main/java/kr/or/ksmart/ksmart_layout/controller/UserController.java
@@ -15,46 +15,41 @@ import kr.or.ksmart.ksmart_vo.User;
 @Controller //UserController 클래스가 컨트롤러임을 인지시켜주는 어노테이션
 public class UserController {
 
-	@Autowired UserService userService; //UserService 클래스 의존성 주입하는 어노테이션
+	@Autowired private UserService userService; //UserService 클래스 의존성 주입하는 어노테이션
 	
-	// defalut.html 에서 회원가입을 클릭해 userInsert url을 요청받으면 회원가입 화면이 떠야 하는게 목표이다
-	// get방식으로 요청받아서 아래의 userInsert() 실행되고 /user/uinsert/userInsert으로 요청 처리된 값을 화면에 뿌려준다
-	@GetMapping("/userInsert")
-	public String userInsert() {
-		return "/user/uinsert/userInsert";
+	// 회원검색결과화면에서 삭제버튼을 누르면 해당id값을 담은채 삭제페이지로 이동하는 것이 목표
+	// get방식으로 userDelete url 요청을 받을 때 각 행의 id값을 받아와 userDelete 메서드 실행
+	@GetMapping("/userDelete")
+	public String userDelete(@RequestParam(value="userId") String userId
+							,Model model) {
+		System.out.println(userId +"<--userDelete UserController"); //userId를 제대로 받아오는지 확인
+		// userUpdate화면띄울때와 동일한 패턴이다
+		// User 데이터 타입 리턴값을 userById라는 이름의 키에 담고 삭제화면으로 이동해서 아이디만 삭제화면에 뿌려줄것이다
+		model.addAttribute("userById", userService.userUpdateById(userId));
+		return "/user/uDelete/userDelete";
 	}
 	
-	// 회원가입form에서 정보를 입력하고 회원가입버튼을 눌러 /user/uinsert/userInsert에서 userInsert url을 요청하면 
-	// form에서 post방식으로 /userInsert url요청받으면 매개변수에 User클래스데이터 타입으로 입력한 회원가입 정보를 
-	// 매개변수 user에 담아 userInsert(User user) 메서드를 실행시키면서 service단으로 요청을 넘긴다
-	@PostMapping("/userInsert")  
-	public String userInsert(User user) {
-		userService.userInsert(user);
-		// userService.userInsert(user) 실행으로 정수값을 리턴받았지만 리턴값을 재사용할 필요가 없으니
-		// 처리과정을 끝마치면 되고 데이터 흐름이 막다른길에 도달했으므로  redirect를 사용해 localhost로 돌아가게끔 한다  
-		return "redirect:/";
+	//회원수정페이지에서 수정할 데이터들을 입력하고 정보수정하기버튼을 누르면 db에 수정사항이 적용되는 것이 목표
+	// post방식으로 수정데이터가 userUpdate 요청을 통해 controller단에 전달되어
+	// 매개변수에 user데이터타입으로 선언된 객체참조변수 user가 담겨서 userUpdate 메서드 실행
+	@PostMapping("/userUpdate")
+	public String userUpdate(User user) {
+		System.out.println(user +"<-- UserController userUpdate");//정보수정하기버튼을 누르면 값들이 제대로 보내지는지 확인
+		//service단의 userUpdate메서드 호출을 요청한다
+		//수정처리가 되면 업무흐름은 끝이므로 호출하는것으로 끝내고 리스트화면으로 리다이렉트 시켜서 수정처리가 되는지 확인할 수 있도록 한다
+		userService.userUpdate(user);
+		return "redirect:/userList";
 	}
-	
-	// 회원관리 -> 회원전체목록을 클릭하면 회원목록을 화면에 보이도록 하는게 목표
-	// get방식으로  /userList url 요청을 받으면 String userList() 실행되고 클릭하면 화면에 뿌려줘야되니까
-	// /user/ulist에 있는 userList.html로 요청 처리된 값을 보낸다 
-	@GetMapping("/userList")
-	public String userList(Model model) {
-		List<User> uList = userService.userList();
-		model.addAttribute("uList", uList);
-		//service단에서 실행된 메서드 리턴값을 List<User> 클래스 데이터 타입으로 선언된 uList 변수에 담는다
-		//uList 변수에 담긴 값을 "uList"라고 정한 키의 값에 할당한다
-		return "/user/ulist/userList";
+	// 회원검색결과화면에서 수정버튼을 누르면 해당id의 정보들을 화면에 보여주는 것이 목표
+	// get방식으로 userUpdate url을 요청받을때 각 행의 id값을 가져와서 userUpdateById() 실행
+	@GetMapping("/userUpdate")
+	public String userUpdateById(@RequestParam(value="userId") String userId
+							,Model model) {
+		//service단에서 요청처리된 리턴값을 model객체에 담는데 "userById"라는 명칭으로 선언한다.
+		model.addAttribute("userById", userService.userUpdateById(userId));
+		
+		return "/user/uUpdate/userUpdate";
 	}
-	
-	// 회원관리 -> 회원검색을 클릭하면 검색화면이 뜨도록 하는게 목표
-	// get방식으로  /userSearch url 요청을 받으면String userSearch() 실행되고 클릭하면 화면에 뿌려줘야되니까
-	// /user/usearch/userSearch.html으로 요청 처리된 값을 보낸다
-	@GetMapping("/userSearch")
-	public String userSearch() {
-		return "/user/usearch/userSearch";
-	}
-	
 	// 회원검색화면에서 검색버튼을 클릭하면 조건에 맞는 목록이 뜨게 하는 것이 목표
 	// post방식으로 /userSearch url요청받으면 userSearch()에 매개변수로 uk, uv에 입력된 값이 담긴채 실행
 	@PostMapping("/userSearch")
@@ -69,14 +64,41 @@ public class UserController {
 		//uList 변수에 담긴 값을 "uList"라고 정한 키의 값에 할당한다
 		return "/user/usearch/userSearch";
 	}
-	// 회원검색결과화면에서 수정버튼을 누르면 해당id의 정보들을 화면에 보여주는 것이 목표
-	// get방식으로 userUpdate url을 요청받을때 각 행의 id값을 가져와서 userUpdateById() 실행
-	@GetMapping("/userUpdate")
-	public String userUpdateById(@RequestParam(value="userId") String userId
-							,Model model) {
-		//service단에서 요청처리된 리턴값을 model객체에 담는데 "userById"라는 명칭으로 선언한다.
-		model.addAttribute("userById", userService.userUpdateById(userId));
-		
-		return "/user/uUpdate/userUpdate";
+	// 회원관리 -> 회원검색을 클릭하면 검색화면이 뜨도록 하는게 목표
+	// get방식으로  /userSearch url 요청을 받으면String userSearch() 실행되고 클릭하면 화면에 뿌려줘야되니까
+	// /user/usearch/userSearch.html으로 요청 처리된 값을 보낸다
+	@GetMapping("/userSearch")
+	public String userSearch() {
+		return "/user/usearch/userSearch";
 	}
+	// 회원관리 -> 회원전체목록을 클릭하면 회원목록을 화면에 보이도록 하는게 목표
+	// get방식으로  /userList url 요청을 받으면 String userList() 실행되고 클릭하면 화면에 뿌려줘야되니까
+	// /user/ulist에 있는 userList.html로 요청 처리된 값을 보낸다 
+	@GetMapping("/userList")
+	public String userList(Model model) {
+		List<User> uList = userService.userList();
+		model.addAttribute("uList", uList);
+		//service단에서 실행된 메서드 리턴값을 List<User> 클래스 데이터 타입으로 선언된 uList 변수에 담는다
+		//uList 변수에 담긴 값을 "uList"라고 정한 키의 값에 할당한다
+		return "/user/ulist/userList";
+	}
+	
+	// 회원가입form에서 정보를 입력하고 회원가입버튼을 눌러 /user/uinsert/userInsert에서 userInsert url을 요청하면 
+		// form에서 post방식으로 /userInsert url요청받으면 매개변수에 User클래스데이터 타입으로 입력한 회원가입 정보를 
+		// 매개변수 user에 담아 userInsert(User user) 메서드를 실행시키면서 service단으로 요청을 넘긴다
+		@PostMapping("/userInsert")  
+		public String userInsert(User user) {
+			userService.userInsert(user);
+			// userService.userInsert(user) 실행으로 정수값을 리턴받았지만 리턴값을 재사용할 필요가 없으니
+			// 처리과정을 끝마치면 되고 데이터 흐름이 막다른길에 도달했으므로  redirect를 사용해 localhost로 돌아가게끔 한다  
+			return "redirect:/";
+		}
+		
+	// defalut.html 에서 회원가입을 클릭해 userInsert url을 요청받으면 회원가입 화면이 떠야 하는게 목표이다
+	// get방식으로 요청받아서 아래의 userInsert() 실행되고 /user/uinsert/userInsert으로 요청 처리된 값을 화면에 뿌려준다
+	@GetMapping("/userInsert")
+	public String userInsert() {
+		return "/user/uinsert/userInsert";
+	}
+		
 }
--- ksmart_layout2/src/main/java/kr/or/ksmart/ksmart_layout/mapper/UserMapper.java
+++ ksmart_layout2/src/main/java/kr/or/ksmart/ksmart_layout/mapper/UserMapper.java
@@ -8,20 +8,26 @@ import kr.or.ksmart.ksmart_vo.User;
 
 @Mapper // UserMapper 인터페이스가 mapping 객체임을 인지시켜주는 어노테이션 
 public interface UserMapper {
-	// UserMapper.xml에서 id가 userInsert인 쿼리문을 호출하는 메서드 
-	// 결과값이 행 1개가 추가되는 것이기에 정수값 1이 리턴될 테니 메서드 데이터타입을 int로 적어준다.
-	public int userInsert(User user);
+
+	// Usermapper.xml에서 id가 userUpdate인 쿼리문을 호출하는 메서드
+	// 결과값에 행 하나가 update되고 정수값 1이 리턴될테니 메서드 데이터 타입은 int로 선언
+	public int userUpdate(User user);
 	
-	// UserMapper.xml에서 id가 userList인 쿼리문을 호출하는 메서드
-	// 결과값이 객체들의 배열형태로 나타나기에 List<User>로 메서드 데이터타입을 선언한다.
-	public List<User> userList();
+	// UserMapper.xml에서 id가 userUpdateById인 쿼리문을 호출하는 메서드
+	// 결과값이 행 하나로 나오기때문에 User 데이터타입으로 메서드 선언한다.
+	public User userUpdateById(String userId);
 	
 	// UserMapper.xml에서 id가 userSearch인 쿼리문을 호출하는 메서드
 	// 결과값이 행 하나로 끝난다면 User 데이터타입으로 선언해도 상관없지만
 	// 객체들의 배열형태로 나타날 수도 있기 때문에 List<User>로 메서드 데이터타입을 선언한다.
 	public List<User> userSearch(String uk, String uv);
 	
-	// UserMapper.xml에서 id가 userUpdateById인 쿼리문을 호출하는 메서드
-	// 결과값이 행 하나로 나오기때문에 User 데이터타입으로 메서드 선언한다.
-	public User userUpdateById(String userId);
+	// UserMapper.xml에서 id가 userList인 쿼리문을 호출하는 메서드
+	// 결과값이 객체들의 배열형태로 나타나기에 List<User>로 메서드 데이터타입을 선언한다.
+	public List<User> userList();
+	
+	// UserMapper.xml에서 id가 userInsert인 쿼리문을 호출하는 메서드 
+	// 결과값이 행 1개가 추가되는 것이기에 정수값 1이 리턴될 테니 메서드 데이터타입을 int로 적어준다.
+	public int userInsert(User user);
+	
 }
